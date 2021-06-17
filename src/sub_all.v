0cf42714a353e60605706982f77f6b204a97caea
Aadreesh <aadreeshsahu@gmail.com>
2021-06-17 13:18:09 +0530

    updated

--- src/sub_all.v
+++ src/sub_all.v
@@ -2,20 +2,21 @@
 `include "exor_all.v"
 `include "or.v"
 `include "and.v"
-
+`include "not_all.v"
 module sub_all #(parameter N = 32)
 
 (
-	input [31:0]a, b,
+	input [31:0]a, d,
 	input c,
 	output [31:0]out, 
 	output cout
 );
 	genvar index;
-	wire [31:0] G, P;
-	wire [31:0] C;
-	wire temp;
-
+	wire [31:0] G, P, b;
+	wire [31:0] C, E;
+	wire temp, temp2;
+	not_all not2(E[31:0], d[31:0]);
+	add_all add2(E[31:0], 32'b1, 1'b0, b[31:0], temp2);
 	assign C[0] = c;
 
 	and_all Cgen(a[31:0], b[31:0], G[31:0]);
